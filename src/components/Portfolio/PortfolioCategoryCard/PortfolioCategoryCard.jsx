fc9166b5890f0b6ab284f5e3150a711c1913e86e
Roberto <robertogianocca@outlook.com>
2025-01-12 21:11:50 +0100

    Update Card layout and add 2xs text.

--- src/components/Portfolio/PortfolioCategoryCard/PortfolioCategoryCard.jsx
+++ src/components/Portfolio/PortfolioCategoryCard/PortfolioCategoryCard.jsx
@@ -17,11 +17,16 @@ export default function PortfolioCategoryCard({
         className={`absolute left-0 top-0 pr-1 w-0 h-0 border-l-[28px] border-b-[28px] border-b-transparent border-t-transparent ${addClass}`}
       ></div> */}
         <h2 className="font-courier font-bold text-lg xl:text-xl">{title}</h2>
+        {/* ---------- SHORT DESCRIPTION DESKTOP  ----------  */}
         <div className="hidden md:block md:min-h-[7rem] lg:min-h-[10rem] xl:min-h-[6rem] text-xs xl:text-xs pb-4 font-semibold opacity-85">
           {shortDescription}
         </div>
         <div className="group-hover:saturate-[1.1] flex flex-col gap-2 lg:gap-4">
           <Image src={cover} width={600} height={600} quality={50} alt={coverAlt} />
+        </div>
+        {/* ---------- SHORT DESCRIPTION MOBILE  ----------  */}
+        <div className="md:hidden text-2xs md:text-xs pb-4 font-semibold opacity-85">
+          {shortDescription}
         </div>
       </div>
     </Link>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,20 +21,21 @@ const config = {
         home: "4 / 3",
       },
       fontSize: {
-        xs: ["0.868rem", "1.1875rem"], // 13.89px -> 0.868rem, 19px -> 1.1875rem
+        "2xs": ["0.694rem", "0.950rem"],
+        xs: ["0.868rem", "1.1875rem"],
         sm: [
-          "1.042rem", // 16.67px -> 1.042rem
+          "1.042rem",
           {
-            lineHeight: "1.375rem", // 22px -> 1.375rem
+            lineHeight: "1.375rem",
             letterSpacing: "0.0em",
           },
         ],
-        base: ["1.25rem", "1.6875rem"], // 20px -> 1.25rem, 27px -> 1.6875rem
-        lg: ["1.5rem", "normal"], // 24px -> 1.5rem
-        xl: ["1.8rem", "normal"], // 28.8px -> 1.8rem
-        "2xl": ["2.16rem", "normal"], // 34.56px -> 2.16rem
-        "3xl": ["2.592rem", "normal"], // 41.47px -> 2.592rem
-        "4xl": ["3.11rem", "normal"], // 49.77px -> 3.11rem
+        base: ["1.25rem", "1.6875rem"],
+        lg: ["1.5rem", "normal"],
+        xl: ["1.8rem", "normal"],
+        "2xl": ["2.16rem", "normal"],
+        "3xl": ["2.592rem", "normal"],
+        "4xl": ["3.11rem", "normal"],
       },
       fontFamily: {
         courier: ["var(--customCourier)"],
